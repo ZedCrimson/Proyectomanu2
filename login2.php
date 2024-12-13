2c7b0f8f4c0a582a69b8d37c1d02b5168fa52fb8
unknown <sgalanbrenes@gmail.com>
2024-12-13 12:10:31 +0100

    Terminado

--- login2.php
+++ login2.php
@@ -31,33 +31,7 @@ require 'conexion.php';
 $email = $_POST['email'];
 $contraseña = $_POST['contraseña'];
 
-$id = "SELECT Id_usuario from tabla_usuarios where Correo_Electrónico = '$email' and contraseña = '$contraseña'";
-$resultadoid = $conexion->query($id);
-if ($resultadoid->num_rows > 0) {
-    $fila = $resultadoid->fetch_assoc();
-    $valorid = $fila['Id_usuario'];
-}
 
-$nombreusu= "SELECT Nombre from tabla_usuarios where Correo_Electrónico = '$email' and contraseña = '$contraseña'";
-$nombreusuario = $conexion->query($nombreusu);
-if ($nombreusuario->num_rows > 0) {
-    $fila = $nombreusuario->fetch_assoc();
-    $valornombre = $fila['Nombre'];
-}
-
-$apellidousu= "SELECT Apellido from tabla_usuarios where Correo_Electrónico = '$email' and contraseña = '$contraseña'";
-$apellidousuario = $conexion->query($apellidousu);
-if ($apellidousuario->num_rows > 0) {
-    $fila = $apellidousuario->fetch_assoc();
-    $valorapellido = $fila['Apellido'];
-}
-
-session_start();
-
-
-$_SESSION['id_usuario'] = $valorid;
-$_SESSION['nombre_usu'] = $valornombre;
-$_SESSION['apellido_usu'] = $valorapellido;
 
 
 $sql = "SELECT * FROM tabla_usuarios where Correo_electrónico='$email' and contraseña='$contraseña' LIMIT 1";
@@ -65,6 +39,33 @@ $resultado = $conexion->query($sql);
 
 $fila = $resultado->fetch_assoc();
 if ($fila){
+    $id = "SELECT Id_usuario from tabla_usuarios where Correo_Electrónico = '$email' and contraseña = '$contraseña'";
+    $resultadoid = $conexion->query($id);
+    if ($resultadoid->num_rows > 0) {
+        $fila = $resultadoid->fetch_assoc();
+        $valorid = $fila['Id_usuario'];
+    }
+
+    $nombreusu= "SELECT Nombre from tabla_usuarios where Correo_Electrónico = '$email' and contraseña = '$contraseña'";
+    $nombreusuario = $conexion->query($nombreusu);
+        if ($nombreusuario->num_rows > 0) {
+            $fila = $nombreusuario->fetch_assoc();
+             $valornombre = $fila['Nombre'];
+        }
+
+    $apellidousu= "SELECT Apellido from tabla_usuarios where Correo_Electrónico = '$email' and contraseña = '$contraseña'";
+    $apellidousuario = $conexion->query($apellidousu);
+        if ($apellidousuario->num_rows > 0) {
+        $fila = $apellidousuario->fetch_assoc();
+        $valorapellido = $fila['Apellido'];
+    }
+
+    session_start();
+
+
+    $_SESSION['id_usuario'] = $valorid;
+    $_SESSION['nombre_usu'] = $valornombre;
+    $_SESSION['apellido_usu'] = $valorapellido;
     header("location: menu.php");
 }else{
     echo "<div class='d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-center' style='width:100%'>";
